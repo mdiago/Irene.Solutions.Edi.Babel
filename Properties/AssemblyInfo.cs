615250324affb863e07583618d250dd41ce52756
Manuel Diago Garcia <manuel@manuelyelena.com>
2021-06-03 10:24:43 +0200

    Actualiza datos
    Actualiza datos

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Irene Solutions SL")]
 [assembly: AssemblyProduct("Irene.Solutions.Edi.Babel")]
-[assembly: AssemblyCopyright("Copyright ©  Irene Solutions SL 2019")]
+[assembly: AssemblyCopyright("Copyright ©  Irene Solutions SL 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Puede especificar todos los valores o usar los números de compilación y de revisión predeterminados
 // mediante el carácter "*", como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.1")]
+[assembly: AssemblyFileVersion("1.0.1.1")]
